4b0c76009932fb7df47a46e1561c4a5f1da9865b
jellynina <jellynina@gmail.com>
2020-04-03 22:28:24 +0800

    Lean Page with template

---
+++ note.md
@@ -0,0 +1,8 @@
+# ~重新學WP架站~
+
+1. 下載一包
+
+~~決定還是用Vue比較快，WP?改天~~
+
+使用Vue
+使用[LOLA THEME](https://creativemarket.com/Dorkoy/724298-Lola-BlogStore-Portfolio)
---
+++ template/nav.vue
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-transparent navbar-fixed-top animated fadeInDown" >
+      <div class="alert bg-gray alert-dismissible text-center" role="alert" id="alert" style="display: none;">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+          <p style="color: #222;">Psst! Surprise your readers with <strong>alerts</strong> like this one <a href="" class="btn btn-dark btn-sharp" style="margin-left: 10px;">Buy Lola</a></p>
+      </div>
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">
+            <img class="logo-light" alt="Brand" src="assets/images/content/logo-icon-light.png">
+            <img class="logo-dark" alt="Brand" src="assets/images/content/logo-icon-dark.png">
+          </a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Home <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                  <li><a href="index.php">Image Slider</a></li>
+                  <li><a href="home-2.php">Single Image</a></li>
+                  <li><a href="home-3.php">Color Background</a></li>
+                  </ul>
+              </li>
+              <li><a href="about.php">About</a></li>
+              <li><a href="folio.php">Folio</a></li>
+              <li><a href="blog.php">Blog</a></li>
+              <li><a href="store.php">Store</a></li>
+              <li><a href="contact.php">Contact</a></li>
+              <li><a class="btn btn-bordered-light btn-sharp" href="#">Buy Lola</a></li>
+          </ul>
+        </div>
+      </div>
+</nav>
